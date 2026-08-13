9b20231e6f6a851013971a398cc9ad9d023e3bc7
sahilgaut1404 <daushyant246@gmail.com>
2026-08-13 14:13:58 +0530

    Update agents.py

--- agents.py
+++ agents.py
@@ -1,5 +1,5 @@
 from langchain.agents import create_agent
-from langchain_openai import ChatOpenAI
+from langchain_mistralai import ChatMistralAI
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_core.output_parsers import StrOutputParser
 from tools import web_search , scrape_url 
@@ -8,7 +8,9 @@ from dotenv import load_dotenv
 load_dotenv()
 
 #model setup 
-llm = ChatOpenAI(model = "gpt-4o-mini",temperature=0)
+llm = ChatMistralAI(
+    model="mistral-small-2603",
+)
 
 
 #1st agent 
